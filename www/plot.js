e461b9549cd0c484f1354f623cf450167bd345e3
naush <lihsuan@8thlight.com>
2011-07-01 23:25:18 -0500

    correct plot div id and style

--- www/plot.js
+++ www/plot.js
@@ -10,32 +10,38 @@ plot.blank = function(text) {
 
 
 plot.plot = function(qa, id, s, elements, callback) {
-  var root716 = $("<span>");
+  var root722 = $("<span>");
   var subs__ = new mobl.CompSubscription();
   
-  var node37 = $("<span>");
-  node37.attr('id', "plot-wrapper");
+  var node42 = $("<span>");
+  node42.attr('id', "plot-wrapper");
   
-  var result__ = plot.blank(qa.get().plotHtml) ? "<div id=id class=s></div>" : qa.get().plotHtml;
-  var tmp681 = mobl.ref(result__);
+  var result__ = plot.blank(qa.get().plotHtml) ? "<div id=" + id.get() + " class=" + s.get() + "></div>" : qa.get().plotHtml;
+  var tmp684 = mobl.ref(result__);
+  subs__.addSub(id.addEventListener('change', function() {
+    tmp684.set(plot.blank(qa.get().plotHtml) ? "<div id=" + id.get() + " class=" + s.get() + "></div>" : qa.get().plotHtml);
+  }));
+  subs__.addSub(s.addEventListener('change', function() {
+    tmp684.set(plot.blank(qa.get().plotHtml) ? "<div id=" + id.get() + " class=" + s.get() + "></div>" : qa.get().plotHtml);
+  }));
   subs__.addSub(mobl.ref(qa, 'plotHtml').addEventListener('change', function() {
-    tmp681.set(plot.blank(qa.get().plotHtml) ? "<div id=id class=s></div>" : qa.get().plotHtml);
+    tmp684.set(plot.blank(qa.get().plotHtml) ? "<div id=" + id.get() + " class=" + s.get() + "></div>" : qa.get().plotHtml);
   }));
   
-  var nodes654 = $("<span>");
-  node37.append(nodes654);
-  subs__.addSub((mobl.html)(tmp681, function(_, callback) {
-    var root717 = $("<span>");
+  var nodes657 = $("<span>");
+  node42.append(nodes657);
+  subs__.addSub((mobl.html)(tmp684, function(_, callback) {
+    var root723 = $("<span>");
     var subs__ = new mobl.CompSubscription();
-    callback(root717); return subs__;
+    callback(root723); return subs__;
     return subs__;
   }, function(node) {
-    var oldNodes = nodes654;
-    nodes654 = node.contents();
-    oldNodes.replaceWith(nodes654);
+    var oldNodes = nodes657;
+    nodes657 = node.contents();
+    oldNodes.replaceWith(nodes657);
   }));
-  root716.append(node37);
-  callback(root716); return subs__;
+  root722.append(node42);
+  callback(root722); return subs__;
   
   
   return subs__;

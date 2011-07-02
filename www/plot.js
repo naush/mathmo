a196318cd141fcc2e91f8b305c6e98aa07a71f57
naush <lihsuan@8thlight.com>
2011-07-01 23:18:48 -0500

    remove duplicated div

--- www/plot.js
+++ www/plot.js
@@ -10,57 +10,32 @@ plot.blank = function(text) {
 
 
 plot.plot = function(qa, id, s, elements, callback) {
-  var root538 = $("<span>");
+  var root716 = $("<span>");
   var subs__ = new mobl.CompSubscription();
   
-  var node28 = $("<span>");
-  node28.attr('id', "plot-wrapper");
+  var node37 = $("<span>");
+  node37.attr('id', "plot-wrapper");
   
-  
-  var node29 = $("<div>");
-  
-  var ref28 = id;
-  if(ref28.get() !== null) {
-    node29.attr('id', ref28.get());
-    subs__.addSub(ref28.addEventListener('change', function(_, ref, val) {
-      node29.attr('id', val);
-    }));
-    
-  }
-  subs__.addSub(ref28.rebind());
-  
-  var ref29 = s;
-  if(ref29.get() !== null) {
-    node29.attr('class', ref29.get());
-    subs__.addSub(ref29.addEventListener('change', function(_, ref, val) {
-      node29.attr('class', val);
-    }));
-    
-  }
-  subs__.addSub(ref29.rebind());
-  
-  var result__ = plot.blank(qa.get().plotHtml) ? "" : qa.get().plotHtml;
-  var tmp515 = mobl.ref(result__);
+  var result__ = plot.blank(qa.get().plotHtml) ? "<div id=id class=s></div>" : qa.get().plotHtml;
+  var tmp681 = mobl.ref(result__);
   subs__.addSub(mobl.ref(qa, 'plotHtml').addEventListener('change', function() {
-    tmp515.set(plot.blank(qa.get().plotHtml) ? "" : qa.get().plotHtml);
+    tmp681.set(plot.blank(qa.get().plotHtml) ? "<div id=id class=s></div>" : qa.get().plotHtml);
   }));
   
-  var nodes491 = $("<span>");
-  node29.append(nodes491);
-  subs__.addSub((mobl.html)(tmp515, function(_, callback) {
-    var root539 = $("<span>");
+  var nodes654 = $("<span>");
+  node37.append(nodes654);
+  subs__.addSub((mobl.html)(tmp681, function(_, callback) {
+    var root717 = $("<span>");
     var subs__ = new mobl.CompSubscription();
-    callback(root539); return subs__;
+    callback(root717); return subs__;
     return subs__;
   }, function(node) {
-    var oldNodes = nodes491;
-    nodes491 = node.contents();
-    oldNodes.replaceWith(nodes491);
+    var oldNodes = nodes654;
+    nodes654 = node.contents();
+    oldNodes.replaceWith(nodes654);
   }));
-  node28.append(node29);
-  root538.append(node28);
-  callback(root538); return subs__;
-  
+  root716.append(node37);
+  callback(root716); return subs__;
   
   
   return subs__;
